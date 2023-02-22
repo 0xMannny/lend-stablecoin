dbef6d1ac7dc6dd326e13b908ef2bceb4121ad5c
0xMannny <103461481+0xMannny@users.noreply.github.com>
2023-02-22 11:09:07 -0800

    Update test_factory.py

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -5,5 +5,5 @@ import boa
 def test_use_factory():
     s = boa.load_partial("contracts/tests/ERC20.vy")
     blueprint = s.deploy_as_blueprint()
-    factory = boa.load("contracts/tests/Factory.vy", blueprint)
+    factory = boa.load("contracts/tests/TestFactory.vy", blueprint)
     s.at(factory.create_new_erc20("token", "TKN", 18, 10**18))
